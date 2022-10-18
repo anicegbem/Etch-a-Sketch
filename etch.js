cf8ded7530c7c8cda853e6618f87fcaddad889c3
anicegbem <matthewdlama4@gmail.com>
2022-10-18 20:45:45 +0100

    create and position the 16x16 grid

--- etch.js
+++ etch.js
@@ -1,1 +1,10 @@
 let container = document.getElementById("container");
+for(let i = 1; i <= 16; i++) {
+    let box = document.createElement('div');
+    container.appendChild(box);
+    for(let i = 1; i <= 16; i++) {
+        let inner = document.createElement('div');
+        inner.setAttribute('class', 'inner');
+        box.appendChild(inner);
+    }
+}
