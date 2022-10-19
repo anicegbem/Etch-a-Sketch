23784f365a19663f10306a812dc2d4ff729166cf
anicegbem <matthewdlama4@gmail.com>
2022-10-19 12:27:40 +0100

    add hover effect on grid

--- etch.js
+++ etch.js
@@ -1,4 +1,6 @@
 let container = document.getElementById("container");
+
+
 for(let i = 1; i <= 16; i++) {
     let box = document.createElement('div');
     container.appendChild(box);
@@ -7,4 +9,18 @@ for(let i = 1; i <= 16; i++) {
         inner.setAttribute('class', 'inner');
         box.appendChild(inner);
     }
-}
+};
+
+let cells = document.getElementsByClassName("inner");
+let content = Array.from(cells);
+
+content.forEach(cell => {
+    cell.addEventListener('mouseover', function putPaint() {
+        cell.style.backgroundColor = "red";
+    });
+    /*cell.addEventListener('mouseout', function removePaint() {
+        cell.style.backgroundColor = "transparent";
+    })*/
+});
+
+
